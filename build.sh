1646da1a9d275129e23459719fff49587a10adc7
onoie <onoie3@gmail.com>
2017-09-29 13:07:07 +0900

    init

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+go build fg.go
+echo "complete"
+
---
+++ get.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+go get github.com/mattn/go-sqlite3
+go get github.com/google/go-github/github
+go get golang.org/x/oauth2
+go get github.com/davecgh/go-spew/spew
+go get github.com/ktrysmt/go-bitbucket
+go get github.com/go-gl/gl/v3.3-core/gl
+go get github.com/go-gl/glfw/v3.1/glfw
